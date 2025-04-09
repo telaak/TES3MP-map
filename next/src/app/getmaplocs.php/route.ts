7e8c9d12e8677c6c4e8d1ed2c91ccb579cd9e2f1
Teemu L. <teemulaaks@gmail.com>
2025-04-09 13:05:46 +0300

    replace empty xml route with env variable

--- next/src/app/getmaplocs.php/route.ts
+++ next/src/app/getmaplocs.php/route.ts
@@ -2,10 +2,15 @@ import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
-    const query = request.nextUrl.searchParams;
-    const xml = await fetch(
-      `https://en.uesp.net/maps/getmaplocs.php?${query}`
-    ).then((res) => res.text());
+    let xml = `<locations><rowcount totalrows="0" rowcount="0" startrow="0"/></locations>`;
+
+    if (!process.env.HIDE_LOCATIONS) {
+      const query = request.nextUrl.searchParams;
+      xml = await fetch(
+        `https://en.uesp.net/maps/getmaplocs.php?${query}`
+      ).then((res) => res.text());
+    }
+
     return new Response(xml, {
       headers: {
         "Content-Type": "text/xml",
