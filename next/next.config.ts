d4f753590337f076f44f81f1845137484b2d75c0
Teemu L. <teemulaaks@gmail.com>
2025-10-07 14:53:02 +0300

    unify routes and use next rewrites

--- next/next.config.ts
+++ next/next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/assets/maps/:path*",
+        destination: "https://gamemap.uesp.net/assets/maps/:path*",
+      },
+      {
+        source: "/db/gamemap.php",
+        destination: "/gamemap",
+      },
+      {
+        source: "/mw/db/gamemap.php",
+        destination: "/gamemap",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- next/src/app/gamemap/route.ts
+++ next/src/app/gamemap/route.ts
@@ -1,14 +1,14 @@
 import { handleGamemapRequest } from "@/functions";
 import { NextRequest } from "next/server";
 
+/**
+ * Unified gamemap endpoint.
+ */
 export async function GET(request: NextRequest) {
   try {
     const response = await handleGamemapRequest(request);
-
     return Response.json(response || []);
   } catch (error) {
-    return new Response(error as string, {
-      status: 500,
-    });
+    return new Response((error as Error).message || "Internal error", { status: 500 });
   }
 }
